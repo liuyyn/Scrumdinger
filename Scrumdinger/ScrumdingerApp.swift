89c55023ab02d2703c66ad8190a37256267d8e5c
Yu Yun Liu <yu.yun.liu@mail.mcgill.ca>
2023-06-20 14:21:13 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Yu Yun Liu on 2023-06-20.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
